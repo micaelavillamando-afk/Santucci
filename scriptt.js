2372de39ff664f0230b22f5b8a4e254ed486e604
micaelavillamando-afk <micaela.villamando@janer.esc.edu.ar>
2025-11-11 10:21:10 -0300

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,11 @@
+
+comenzar.addEventListener('click', function() {
+    // Cambiá este link por el que quieras abrir:
+    const url = "https://sebadac.github.io/";
+    
+    // Abre el enlace en una nueva pestaña o ventana
+    window.open(url, "_blank");
+  });
+  
+  
+  
